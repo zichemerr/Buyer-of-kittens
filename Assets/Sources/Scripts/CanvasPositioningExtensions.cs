96febc0c1f1784cf1f428da67189fc06d082d382
unknown <zichemerr9@gmail.com>
2024-12-01 17:01:44 +0500

    Added clickEffect and car deleted

---
+++ Assets/Sources/Scripts/CanvasPositioningExtensions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public static class CanvasPositioningExtensions
+{
+    public static Vector3 WorldToCanvasPosition(this Canvas canvas, Vector3 worldPosition, Camera camera)
+    {
+        var viewportPosition = camera.WorldToViewportPoint(worldPosition);
+        return canvas.ViewportToCanvasPosition(viewportPosition);
+    }
+
+    public static Vector3 ScreenToCanvasPosition(this Canvas canvas, Vector2 screenPosition)
+    {
+        var viewportPosition = new Vector2(screenPosition.x / Screen.width, screenPosition.y / Screen.height);
+
+        return canvas.ViewportToCanvasPosition(viewportPosition);
+    }
+
+    public static Vector3 ViewportToCanvasPosition(this Canvas canvas, Vector2 viewportPosition)
+    {
+        var canvasTransform = (RectTransform)canvas.transform;
+
+        var scaled = Vector2.Scale(viewportPosition, canvasTransform.sizeDelta);
+
+        return Vector2.Scale(scaled, canvasTransform.localScale);
+    }
+}
